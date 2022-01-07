cd55b30d3f6d9d30408890a2090284221de56a2b
0x0000011 <90667396+0x0000011@users.noreply.github.com>
2022-01-07 21:16:45 +0300

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+--Loader
+
+if game.PlaceId == 5837160206 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/0x0000011/Project-1/main/_2/ACS_O_S.lua"))()
+
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/0x0000011/Project-1/main/_2/Universal.lua"))()
+end
